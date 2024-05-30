fa9b2222918659b9df926170de9b79a574b9058b
Ritzl Bence <ritzlbencelevente@ktch.hu>
2024-05-30 08:42:58 +0200

    kész

---
+++ src/app.ts
@@ -0,0 +1,49 @@
+class Ellipszoid {
+    sideAInput?: HTMLInputElement | null;
+    sideBInput?: HTMLInputElement | null;
+    sideCInput?: HTMLInputElement | null;
+    areaInput?: HTMLInputElement | null;
+    calcButton?: HTMLButtonElement | null;
+
+
+    constructor() {
+        this.bindHtml();
+        this.HandleEvent();
+
+    }
+
+
+    bindHtml() {
+        this.sideAInput = document.querySelector("#sideA")
+        this.sideBInput = document.querySelector("#sideB")
+        this.sideCInput = document.querySelector("#sideC")
+        this.areaInput = document.querySelector("#area")
+        this.calcButton = document.querySelector("#calcButton")
+    }
+
+    HandleEvent(){
+        this.calcButton?.addEventListener("click", () => {
+            this.startCalc();
+        })
+    }
+
+    startCalc() {
+        const sideA = Number(this.sideAInput?.value);
+        const sideB = Number(this.sideBInput?.value);
+        const sideC = Number(this.sideCInput?.value);
+        const area = this.calcArea(sideA, sideB, sideC);
+        this.renderResult(area)
+    }
+
+    calcArea(sideA: number, sideB: number, sideC: number): number {
+        return (4/3) * Math.PI * sideA * sideB * sideC;
+    }
+
+    renderResult(area: number) {
+        if (this.areaInput) {
+            this.areaInput.value = String(area)
+        }
+    }
+
+}
+new Ellipszoid();
